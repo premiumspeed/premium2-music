d2f14d21b08d9999e1b2a087402ba7c966b18792
premiumspeed <43043161+premiumspeed@users.noreply.github.com>
2018-09-07 17:06:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const queue = new Map();
 const client = new Discord.Client();
 
- 
+
 
 const prefix = "1"
 client.on('message', async msg => { 
